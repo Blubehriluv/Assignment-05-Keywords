c75340d86ffb59f5d4e229439ab6daa89a3313d2
Taylor McKenzie <44045526+Blubehriluv@users.noreply.github.com>
2019-02-07 13:21:33 -0700

    Added Actual Code
    From Class stuff

---
+++ Assignment 05/Keywords/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	for (int x = 1; x < 3; x++)
+	{
+		//Have the program run 3 times.
+	}
+
+
+	enum fields { WORD, HINT, NUM_FIELDS };
+
+	const int NUM_WORDS = 10;
+	const string words[NUM_WORDS][NUM_FIELDS] =
+	{
+		{ "secret",  "classified stuff" },
+		//Add mo
+	};
+}
